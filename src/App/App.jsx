9422186c9ae625a1f6220ab1e6e6524a5e98b597
Santa <s.yushmanov777@gmail.com>
2018-09-18 21:19:54 +0700

    catch response error

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -9,11 +9,13 @@ const Hello = styled.div`
 
 export default ({ doRequest, weather, input, inputHandler, bgimg }) => {
   console.log(weather);
+
   return (
     <Wrapper bg={bgimg}>
       <Hello>Hello Jushka!</Hello>
       <input value={input} onChange={e => inputHandler(e.target.value)} type="text"/>
       <button onClick={doRequest}>Get Weather</button>
+      { weather.error && 'Ошибка, повторите запрос' }
     </Wrapper>
   )
 };
--- src/store/actions.js
+++ src/store/actions.js
@@ -2,5 +2,7 @@ import { GET_WEATHER } from './constants';
 import { fetchWeather, fetchWeatherByCoord } from '../API'
 
 export const getWeather = (data) => dispatch => {
-  (typeof data === 'string' ? fetchWeather : fetchWeatherByCoord)(data).then(payload => dispatch({ type: GET_WEATHER, payload }))
+  (typeof data === 'string' ? fetchWeather : fetchWeatherByCoord)(data)
+    .then(payload => dispatch({ type: GET_WEATHER, payload }))
+    .catch(e => dispatch({ type: GET_WEATHER, payload: { error: 'Bad response' } }))
 }
